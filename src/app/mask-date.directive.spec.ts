c109987d2a3c39231dd928b3b231aedba880f735
nairobi77 <sobakko@gmail.com>
2018-06-06 21:36:14 +0300

    add mask directive

---
+++ src/app/mask-date.directive.spec.ts
@@ -0,0 +1,8 @@
+import { MaskDateDirective } from './mask-date.directive';
+
+describe('MaskDateDirective', () => {
+  it('should create an instance', () => {
+    const directive = new MaskDateDirective();
+    expect(directive).toBeTruthy();
+  });
+});
